f21f46459f08f75f5aa3c78712c86c6f8b067aa9
Ankita Das <ch25b013@smail.iitm.ac.in>
2026-02-03 10:10:17 +0530

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { Box, Heading, Text, Button } from "@chakra-ui/react";
 const Hero = () => {
   return (
     <Box id="home" textAlign="center" py={20} bgImage="url('https://i.ytimg.com/vi/atozjUyQ0Tc/maxresdefault.jpg')"
-     bgSize="cover" bgPosition="center" bgRepeat="no-repeat" minH="100vh"  
+     bgSize="cover" bgPosition="center" bgRepeat="no-repeat" minH="90vh"  
      >
       <Heading fontSize="4xl" color="white">Welcome to My Favorite Series</Heading>
       
